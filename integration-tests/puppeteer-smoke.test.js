fb3cee1b9019f30067f31ac3d3da4dd9aedf395a
Andrei D. Robu <andreidrobu@gmail.com>
2020-10-08 15:31:53 +0100

    hardcode public path to /electric-disks
    This is because the "serve" npm package is not able to accept variables
    in rewrite definitions.

--- integration-tests/puppeteer-smoke.test.js
+++ integration-tests/puppeteer-smoke.test.js
@@ -1,4 +1,3 @@
-// import 'expect-puppeteer'
 
 describe('Electric Disks', () => {
   beforeAll(async () => {
@@ -6,9 +5,8 @@ describe('Electric Disks', () => {
     // TODO refactor this JS error checking functionality out of
     // this test so that it itself can be tested in another test.
     page.on('pageerror', err => errors.push(err))
-    await page.goto('http://localhost:5000/placeholder-path')
+    await page.goto('http://localhost:5000/electric-disks')
   })
-
   it('does not show JS errors after page load', async () => {
     expect(errors).toEqual([])
   })
