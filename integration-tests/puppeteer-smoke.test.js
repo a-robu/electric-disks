17e1ecb09c0545217fd57b629423af6a4a1b962e
Andrei D. Robu <andreidrobu@gmail.com>
2020-10-08 15:13:36 +0100

    add TODO: test the JS error listing test

--- integration-tests/puppeteer-smoke.test.js
+++ integration-tests/puppeteer-smoke.test.js
@@ -3,6 +3,8 @@
 describe('Electric Disks', () => {
   beforeAll(async () => {
     errors = []
+    // TODO refactor this JS error checking functionality out of
+    // this test so that it itself can be tested in another test.
     page.on('pageerror', err => errors.push(err))
     await page.goto('http://localhost:5000/placeholder-path')
   })
